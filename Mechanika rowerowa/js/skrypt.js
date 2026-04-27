86cf22a7e5cd82d5ba4a633e6deb786d86a3f479
Dabioch Web <psowatypsowaty@gmail.com>
2026-04-27 20:39:21 +0200

    Delete Mechanika rowerowa directory

--- Mechanika rowerowa/js/skrypt.js
+++
@@ -1,7 +0,0 @@
-// MENU MOBILE
-const toggle = document.querySelector(".menu-toggle");
-const nav = document.querySelector(".nav-links");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
